c7f103c2ce62029c5e2348892e808b46fc04682b
Solomon Huckstep <solomonhuckste.dev@gmail.com>
2026-09-21 18:21:23 -0400

    Expanded Series to include all available serios from OCB API and drop Openf1

--- backend/models.go
+++ backend/models.go
@@ -1,37 +1,48 @@
-package main 
+package main
 
 import "time"
 
 type Series string
 
 const (
-	SeriesF1 Series = "F1"
-	SeriesIndyCar Series = "IndyCar"
+	Formula_1      Series = "Formula1"
+	NASCAR         Series = "Nascar"
+	NASCAR_Xfinity Series = "NascarXfinity"
+	NASCAR_Trucks  Series = "NascarTrucks"
+	IndyCar        Series = "Indycar"
+	Moto_GP        Series = "MotoGp"
+	Moto_2         Series = "Moto2"
+	Moto_3         Series = "Moto3"
+	Formula_E      Series = "FormulaE"
+	Formula_2      Series = "Formula2"
+	Formula_3      Series = "Formula3"
+	WEC            Series = "Wec"
+	WRC            Series = "Wrc"
 )
 
 type Driver struct {
-	ID string `json:"id"` 
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	Series Series `json:"series"`
-	DriverNumber *int  `json:"driverNumber,omitempty"`
-	TeamName *string `json:"teamName,omitempty"`
-	Nationality *string `json:"nationality,omitempty"`
+	ID           string  `json:"id"`
+	FirstName    string  `json:"firstName"`
+	LastName     string  `json:"lastName"`
+	Series       Series  `json:"series"`
+	DriverNumber *int    `json:"driverNumber,omitempty"`
+	TeamName     *string `json:"teamName,omitempty"`
+	Nationality  *string `json:"nationality,omitempty"`
 }
 
 type Race struct {
-	ID string  `json:"id"`
-	Series Series `json:"series"`
-	Name string `json:"name"`
-	Date time.Time `json:"date"`
-	Round int `json:"round"`
+	ID     string    `json:"id"`
+	Series Series    `json:"series"`
+	Name   string    `json:"name"`
+	Date   time.Time `json:"date"`
+	Round  int       `json:"round"`
 }
 
 type RaceResults struct {
-	RaceID string `json:"raceId"`
-	DriverID string `json:"driverId"`
-	FinishPosition *int `json:"finishPosition,omitempty"`
-	StartPosition *int `json:"startPosition,omitempty"`
-	Points float64  `json:"points"`
-	Status string `json:"status"`
+	RaceID         string  `json:"raceId"`
+	DriverID       string  `json:"driverId"`
+	FinishPosition *int    `json:"finishPosition,omitempty"`
+	StartPosition  *int    `json:"startPosition,omitempty"`
+	Points         float64 `json:"points"`
+	Status         string  `json:"status"`
 }
--- shared/Pitwall.Shared/Series.cs
+++ shared/Pitwall.Shared/Series.cs
@@ -2,6 +2,17 @@ namespace Pitwall.Shared;
 
 public enum Series
 {
-  F1,
-  IndyCar
+  Formula1,
+  Nascar,
+  NascarXfinity,
+  NascarTrucks,
+  Indycar,
+  MotoGp,
+  Moto2,
+  Moto3,
+  FormulaE,
+  Formula2,
+  Formula3,
+  Wec,
+  Wrc
 }
